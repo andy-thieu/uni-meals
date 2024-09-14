c58bdaf4503dcc9bca556e7463e1c9c04ab8f0f0
andy-thieu <thieuandyhuy@gmail.com>
2024-09-14 22:48:11 +0200

    menu api call

--- app/components/menu-component.tsx
+++ app/components/menu-component.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+'use client'
+
+import React, {useEffect} from "react";
 import styles from "./menu-component.module.css";
+import axios from "axios";
 
 interface MenuProps {
     canteenId: string
 }
 
 const MenuComponent : React.FC<MenuProps> = ({canteenId}) => {
+
+    const [menu, setMenu] = React.useState(null);
+
+    useEffect(() => {
+        axios.get(`https://mensa.gregorflachs.de/api/v1/menue?loadingtype=lazy&canteenId=${canteenId}&startdate=2024-07-12&enddate=2024-07-12`, {
+            headers: {
+                'X-API-KEY': process.env.NEXT_PUBLIC_API_KEY
+            }
+        }).then(response => {
+            console.log(response.data);
+            setMenu(response.data[0].canteenId);
+        }).catch(error => {
+            console.error(error);
+        });
+    }, [canteenId]);
+
     return (
         <div className={styles.main}>
             <h2>Speiseplan</h2>
-            {canteenId}
+            {menu}
         </div>
     );
 };
